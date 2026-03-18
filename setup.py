5ea481389ad192434778eeb05abaf903777a4077
JuanSilva99 <juangusilva99@gmail.com>
2026-03-18 19:29:44 -0300

    update gitignore and setup py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='meu_investimento',
+   name='meu_investimento_JuanSilva99',
    version='0.1',
    packages=find_packages(),
    install_requires=[],
